d75f820448e02d408a79f185a78d203cfd6c34c8
varun-2024 <v.talwar2024@gmail.com>
2025-02-21 21:15:08 +0000

    Ternary operator & Logical Operator

--- practice1/HealthyFoodsList/HealthyFoods/src/App.jsx
+++ practice1/HealthyFoodsList/HealthyFoods/src/App.jsx
@@ -11,9 +11,23 @@ function App() {
     "Milk",
     "Fruits",
   ];
+  //let foodItems = [];
+  //Terinary Operator
+  /* let emptyMessage =
+    foodItems.length === 0 ? (
+      <h3 className="text-center">No Items Found...</h3>
+    ) : null; */
+
+  /*   if (foodItems.length === 0) {
+    return <h3 className="text-center">No Items Found</h3>;
+  } */
   return (
     <React.Fragment>
       <h1 className="text-center">Healthy Foods</h1>
+      {/* Using Logical Operator for Above Statement */}
+      {foodItems.length === 0 && (
+        <h3 className="text-center">No Items Found...</h3>
+      )}
       <ul className="list-group text-center">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
