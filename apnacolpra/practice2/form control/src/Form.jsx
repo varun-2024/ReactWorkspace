d4e8aa8b021178806ad21d1acfaea1c378fd34c5
varun-2024 <v.talwar2024@gmail.com>
2025-06-18 23:02:01 +0100

    Refactor Form component to manage form data using a single state object and streamline input handling

--- apnacolpra/practice2/form control/src/Form.jsx
+++ apnacolpra/practice2/form control/src/Form.jsx
@@ -1,37 +1,56 @@
 import { useState } from "react";
 
 export default function Form() {
-  let [fullName, setFullName] = useState("Hema");
-  let [userName, setUserName] = useState("Hema123");
-  let handleNameChange = (event) => {
+  //let [fullName, setFullName] = useState("Hema");
+  //let [userName, setUserName] = useState("Hema123");
+  /*   let handleNameChange = (event) => {
     setFullName(event.target.value);
   };
   let handleuserNameChange = (event) => {
     setUserName(event.target.value);
+  }; */
+  let [formData, setFormData] = useState({
+    fullName: "",
+    userName: "",
+  });
+
+  let handleInputChange = (event) => {
+    //const { name, value } = event.target;
+    setFormData((prevData) => {
+      return {
+        ...prevData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form submitted with data:", formData);
+    setFormData({ fullName: "", userName: "" });
   };
   return (
     <>
-      <form className="" action="" method="">
+      <form className="" action="" method="" onSubmit={handleSubmit}>
         <label htmlFor="fullName">Full Name</label>
         <input
           type="text"
           id="fullName"
-          name="name"
+          name="fullName"
           placeholder="Enter Full Name"
           className=""
-          value={fullName}
-          onChange={handleNameChange}
+          value={formData.fullName}
+          onChange={handleInputChange}
         />
         <br />
         <label htmlFor="userName">User Name</label>
         <input
           type="text"
           id="userName"
-          name="name"
+          name="userName"
           placeholder="Enter User Name"
           className=""
-          value={userName}
-          onChange={handleuserNameChange}
+          value={formData.userName}
+          onChange={handleInputChange}
         />
         <br />
         <button className="">Submit</button>
