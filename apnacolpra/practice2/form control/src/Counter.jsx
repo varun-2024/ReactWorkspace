15f6ad902c6e8a28312f7349a0c2e22a40f961b5
varun-2024 <v.talwar2024@gmail.com>
2025-06-19 21:30:42 +0100

    Refactor Counter component to manage two independent counters with increment and decrement functionality

--- apnacolpra/practice2/form control/src/Counter.jsx
+++ apnacolpra/practice2/form control/src/Counter.jsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
 export default function Counter() {
+  let [countx, setCountx] = useState(0);
+  let [county, setCounty] = useState(0);
+  let incCountx = () => {
+    setCountx((currCount) => currCount + 1);
+  };
+  let decCountx = () => {
+    setCountx((currCount) => currCount - 1);
+  };
+  let incCounty = () => {
+    setCounty((currCount) => currCount + 1);
+  };
+  let decCounty = () => {
+    setCounty((currCount) => currCount - 1);
+  };
+  useEffect(() => {
+    console.log("Counter Updated");
+  }, []);
   return (
     <>
       <h3 className="">Counter</h3>
+      <h4>X: {countx}</h4>
+      <h4>Y: {county}</h4>
+      <button onClick={incCountx}>X Increment+1</button>
+      <button onClick={decCountx}>X Decrement-1</button>
+      <button onClick={incCounty}>Y Increment+1</button>
+      <button onClick={decCounty}>Y Decrement-1</button>
     </>
   );
 }
