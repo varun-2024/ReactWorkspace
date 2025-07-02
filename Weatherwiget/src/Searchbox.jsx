9377a7bf083de0bbb96d5cc19b8bc189836947dc
varun-2024 <v.talwar2024@gmail.com>
2025-07-02 12:19:19 +0100

    feat: implement Searchbox component and update App component

---
+++ Weatherwiget/src/Searchbox.jsx
@@ -0,0 +1,18 @@
+export default function Searchbox() {
+  return (
+    <>
+      <h1 className="">Weather App</h1>
+      <h2 className="">Search City for Weather</h2>
+      <form className="" action="" method="">
+        <input
+          type="text"
+          id="id"
+          name="name"
+          placeholder="placeholder"
+          className="w-[300px] border border-slate-200 rounded-lg py-3 px-5 outline-none	bg-transparent"
+        />
+        <button className="">Search</button>
+      </form>
+    </>
+  );
+}
