05b42a77a4f5528de9f4ec588003cb17453193e5
varun-2024 <v.talwar2024@gmail.com>
2025-07-02 12:01:25 +0100

    feat: add Alert component with collapse functionality in App component

--- apnacolpra/practice3/materialUI/src/App.jsx
+++ apnacolpra/practice3/materialUI/src/App.jsx
@@ -2,11 +2,21 @@ import "./App.css";
 import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
 import SendIcon from "@mui/icons-material/Send";
+import Alert from "@mui/material/Alert";
+import CheckIcon from "@mui/icons-material/Check";
+import AlertTitle from "@mui/material/AlertTitle";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Collapse from "@mui/material/Collapse";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
 
 function App() {
   let handleClick = () => {
     console.log("Button was Clicked!");
   };
+  const [open, setOpen] = useState(true);
+
   return (
     <>
       <h1 className="">Material UI Demo</h1>
@@ -28,6 +38,47 @@ function App() {
       >
         Click Me2
       </Button>
+
+      <Alert
+        variant="outlined"
+        icon={<CheckIcon fontSize="inherit" />}
+        severity="success"
+        onClose={() => {}}
+      >
+        <AlertTitle>Success</AlertTitle>
+        Here is a gentle confirmation that your action was successful.
+      </Alert>
+
+      <Box sx={{ width: "100%" }}>
+        <Collapse in={open}>
+          <Alert
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+            }
+            sx={{ mb: 2 }}
+          >
+            Click the close icon to see the Collapse transition in action!
+          </Alert>
+        </Collapse>
+        <Button
+          disabled={open}
+          variant="outlined"
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          Re-open
+        </Button>
+      </Box>
     </>
   );
 }
