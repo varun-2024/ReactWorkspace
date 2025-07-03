75ba6b96e11413265b454558e12e508eb34a065a
varun-2024 <v.talwar2024@gmail.com>
2025-07-03 18:29:40 +0100

    fix: reset error state after successful weather data fetch in Searchbox

--- Weatherwiget/src/Searchbox.jsx
+++ Weatherwiget/src/Searchbox.jsx
@@ -37,6 +37,7 @@ export default function Searchbox({ setWeatherInfo }) {
       setCity("");
       let newInfo = await getWeatherInfo();
       setWeatherInfo(newInfo);
+      setError(false);
       /* getWeatherInfo().then((data) => {
       setWeatherInfo(data);
     }); */
