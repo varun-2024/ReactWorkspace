ac8b1b86d2502808dfac18fa02688738edb3162c
varun-2024 <v.talwar2024@gmail.com>
2025-07-03 15:11:36 +0100

    feat: improve error handling in Searchbox component for weather data fetching

--- Weatherwiget/src/Searchbox.jsx
+++ Weatherwiget/src/Searchbox.jsx
@@ -5,35 +5,45 @@ import { useState } from "react";
 
 export default function Searchbox({ setWeatherInfo }) {
   let [city, setCity] = useState("");
+  let [error, setError] = useState(false);
   const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
   let API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
   let getWeatherInfo = async () => {
-    let response = await fetch(API_URL);
-    let data = await response.json();
-    console.log(data);
-    let result = {
-      city,
-      temp: data.main.temp,
-      temp_min: data.main.temp_min,
-      temp_max: data.main.temp_max,
-      humidity: data.main.humidity,
-      feelslike: data.main.feels_like,
-      weather: data.weather[0].description,
-    };
-    console.log(result);
-    return result;
+    try {
+      let response = await fetch(API_URL);
+      let data = await response.json();
+      console.log(data);
+      let result = {
+        city,
+        temp: data.main.temp,
+        temp_min: data.main.temp_min,
+        temp_max: data.main.temp_max,
+        humidity: data.main.humidity,
+        feelslike: data.main.feels_like,
+        weather: data.weather[0].description,
+      };
+      console.log(result);
+      return result;
+    } catch (error) {
+      throw error;
+    }
   };
 
   let handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(city);
-    setCity("");
-    let newInfo = await getWeatherInfo();
-    setWeatherInfo(newInfo);
-    /* getWeatherInfo().then((data) => {
+    try {
+      e.preventDefault();
+      console.log(city);
+      setCity("");
+      let newInfo = await getWeatherInfo();
+      setWeatherInfo(newInfo);
+      /* getWeatherInfo().then((data) => {
       setWeatherInfo(data);
     }); */
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      setError(true);
+    }
   };
   return (
     <>
@@ -61,6 +71,11 @@ export default function Searchbox({ setWeatherInfo }) {
           >
             Search
           </Button>
+          {error && (
+            <p style={{ color: "red" }}>
+              Error fetching weather data. Please try again.
+            </p>
+          )}
         </form>
       </div>
     </>
